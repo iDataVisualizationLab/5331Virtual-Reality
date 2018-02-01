2bb66b329c073f093cf845edb243b25be6572e57
Tommy Dang <nhontuan@gmail.com>
2018-01-31 23:18:57 -0600

    update l2

--- testJS/myscript.js
+++ testJS/myscript.js
@@ -1,3 +1,18 @@
-var cars = ["Saab", "Volvo", "BMW"];
+var cars = [{name: "A", value: 11}, {name:"B", value: 6}, {name: "C", value: 3}];
 
-document.getElementById("item").innerHTML = cars[0];
+cars[2] ={name: "CCC", value: 1234};
+
+cars.sort( function(a, b){
+    if (a.value>b.value)
+        return 1;
+    else
+        return -1;
+});
+
+var text = "<ul>";
+for (var i=0; i<cars.length;i++){
+    text+="<li>"+cars[i].value+"</li>";
+}
+text += "</ul>";
+
+document.getElementById("item").innerHTML = text;
