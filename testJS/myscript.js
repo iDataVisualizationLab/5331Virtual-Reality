f24df2b1d1c957be05eb017bb08116575a0aa35f
Tommy Dang <nhontuan@gmail.com>
2018-02-06 01:30:10 -0600

    udpate l3

--- testJS/myscript.js
+++ testJS/myscript.js
@@ -1,18 +1,23 @@
-var cars = [{name: "A", value: 11}, {name:"B", value: 6}, {name: "C", value: 3}];
+var cars = [{name: "A", value: 111}, {name:"B", value: 12}
+        , {name: "C", value: 3}];
 
-cars[2] ={name: "CCC", value: 1234};
 
-cars.sort( function(a, b){
-    if (a.value>b.value)
-        return 1;
-    else
-        return -1;
-});
+        cars.sort( function(a, b){
+            if (a.value>b.value)
+            return 1;
+          else
+            return -1;
+        });
 
-var text = "<ul>";
-for (var i=0; i<cars.length;i++){
-    text+="<li>"+cars[i].value+"</li>";
-}
-text += "</ul>";
+        console.log("My array: " ,cars);
 
-document.getElementById("item").innerHTML = text;
+        debugger;
+
+        //cars[10] =100;
+        var text = "<ul>";
+        for (var i=0; i<cars.length;i++){
+            text+="<li>"+cars[i].value+"</li>";
+        }
+        text += "</ul>";
+
+    document.getElementById("item").innerHTML = text;
