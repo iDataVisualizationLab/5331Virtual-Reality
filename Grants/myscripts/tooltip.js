1f47f3ba194f190c21121eeb94c6b86abb4a9282
Tommy Dang <nhontuan@gmail.com>
2018-04-18 01:47:38 -0500

    update

--- Grants/myscripts/tooltip.js
+++ Grants/myscripts/tooltip.js
@@ -2,24 +2,42 @@
     // See original documentation for more details on styling: http://labratrevenge.com/d3-tip/
     var tool_tip = d3.tip()
       .attr("class", "d3-tip")
-      .offset([-8, 0])
+      .offset([-8, 200])
       .html(function(d) { 
         str="";
         str+="<table border='0.5px'  style='width:100%'>"
         for (key in d) {
           if (key== "x" || key== "y" || key== "vx" || key== "vy" || key== "fx" || key== "fy" || key== "index"|| key== "SponsorList")
               ;// Do nothing
+          else if (key== "Amount")
+            str+=  "<tr><td> Amount</td> <td>  <span style='color:black'>" + d[key].toLocaleString() + "</span> </td></tr>";
+          else if (key== "Deadline")
+            str+=  "<tr><td> Deadline</td> <td>  <span style='color:black'>" + formatDate(d[key]) + "</span> </td></tr>";
           else if (key== "name")
             str+=  "<tr><td> Title</td> <td>  <span style='color:black'>" + d[key] + "</span> </td></tr>";
-          else if (key== "L ink")
+          else if (key== "Link")
             str+=  "<tr><td>"+key+"</td> <td>  <span style='color:blue'>" + d[key] + "</span> </td></tr>";
           else{
             str+=  "<tr><td>"+key+"</td> <td>  <span style='color:black'>" + d[key] + "</span> </td></tr>";
           }     
         } 
         str+="</table> <br>"
-
-        str+="<span style='color:blue'><b> Please CLICK to go to the page </b></span> "
+        if (d.Link!=undefined)
+          str+="<span style='color:blue'><b> Please CLICK to go to the page </b></span> "
         return str; });
     svg.call(tool_tip);
     
+function formatDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
